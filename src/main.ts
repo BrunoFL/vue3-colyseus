bc82d42255e991bc73b0d2a9e9bc59ec2eae440b
Bruno FL <follet-bruno@hotmail.fr>
2022-05-18 10:45:17 +0200

    base

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ app.use(createPinia());
 
 app.mount("#app");
 console.log(import.meta.env);
-const host = import.meta.env.DEV ? "ws://localhost:3001" : process.env.VUE_APP_HOST;
+const host = import.meta.env.DEV ? "ws://localhost:3001" : document.location.href;
 console.log(host);
 const client = new Colyseus.Client(host);
 
